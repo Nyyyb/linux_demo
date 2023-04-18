06b6cbce8a3d94eca668b1294b44dc37abb5a1bb
nyyyb <1534748620@qq.com>
2023-04-18 22:00:15 +0800

    lesson07

---
+++ lesson07/main.c
@@ -0,0 +1,11 @@
+#include "head.h"
+
+int main()
+{
+    int a = 10;
+    int b = 5;
+    printf("a+b=%d", add(a,b));
+    printf("a-b=%d", sub(a,b));
+    printf("a*b=%d", mul(a,b));
+    printf("a/b=%f", div(a,b));
+}
